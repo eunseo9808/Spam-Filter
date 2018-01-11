5416da45d4f5f3c36192a139d092f3571a57d9c9
Eunseo Lee <eunseo9808@naver.com>
2018-01-11 21:35:42 +0900

    Find Spam from redirections

--- SpamFilter.py
+++ SpamFilter.py
@@ -9,10 +9,46 @@ def findUrl(content):
     url = content[urlStart:urlEnd]
     return url
 
+def checkSpamLink(spamLinkDomains, url):
+    for spamLinkDomain in spamLinkDomains:
+        if not url.find(spamLinkDomain)==-1 :
+            return True
+    return False
+
+
 def isSpam(content, spamLinkDomains, redirectionDepth):
     url=findUrl(content)
 
-isSpam('spam spam https://goo.gl/nVLutc', None, 0)
+    if checkSpamLink(spamLinkDomains, url):
+        return True
+
+    res = requests.get(url)
+    for resp in res.history:
+        if redirectionDepth <= 0:
+            return False
+
+        if resp.status_code == 301 or resp.status_code == 302:
+            redirectionDepth -= 1
+        else:
+            continue
+
+        if resp.url == url: continue
+
+        if checkSpamLink(spamLinkDomains, resp.url):
+            return True
+
+    if checkSpamLink(spamLinkDomains, res.url):
+        return True
+
+    return False
+
+
+
+
+
+
+
+print(isSpam('spam spam https://goo.gl/nVLutc', ['tvtv24.com'], 1))
 
 
 
